2b7f737c702d50da380c13aa2d6517a4b4568b69
Viczmándi Gergely <viczmandi@gmail.com>
2016-04-17 18:31:15 +0200

    First commit/push

---
+++ SequentialCollections/SequentialCollections/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequentialCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+            q.Enqueue("First");
+            q.Enqueue("Second");
+            q.Enqueue("Third");
+            q.Enqueue("Fourth");
+
+            while (q.Count > 0)
+            {
+                object obj = q.Dequeue();
+                Console.WriteLine("From queue: {0}", obj);
+            }
+
+            Console.WriteLine("\n");
+
+            Stack stack = new Stack();
+            stack.Push("First");
+            stack.Push("Second");
+            stack.Push("Third");
+            stack.Push("Fourth");
+
+            while (stack.Count > 0)
+            {
+                object obj = stack.Pop();
+                Console.WriteLine("From stack: {0}", obj);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
